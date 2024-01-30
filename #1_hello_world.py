52130e5c8e29681f014eff5b94a99bd024c95984
Fernanda Ishida <fer.ishida@gmail.com>
2024-01-30 16:46:47 -0300

    #1_hello_world

---
+++ #1_hello_world.py
@@ -0,0 +1,7 @@
+'''Print "Hello, world!" on the screen:'''
+print('Hello, world!')
+'''Ask the user for their name:'''
+'''Get the user name from their keyboard input:'''
+name = input('Whats is your name? ')
+'''Greet the user by their name:'''
+print(f'Hello, {name}!')
